6c48b9a4a2c1a1fdef72be3c838393af2ab985c2
Austin Ziegler <austin@zieglers.ca>
2025-02-13 21:31:52 -0500

    fix: Chezmoi wrapper (1.1.0 candidate)
    - Fix the chezmoi wrapper function by reducing its cleverness.
    - Added `cdsh` as an argument to call the original `chezmoi cd`.
    - Fixed chezmoi completions handling.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,13 @@
 # halostatue/fish-chezmoi Changelog
 
+## 1.1.0 / 2025-02-13
+
+- Fix the chezmoi wrapper function by reducing its cleverness.
+- Added `cdsh` as an argument to call the original `chezmoi cd`.
+- Fixed chezmoi completions handling.
+- Added security checking for workflows and updated workflow security.
+- Added FUNDING.
+
 ## 1.0.0 / 2025-02-09
 
 - Created halostatue/fish-chezmoi
--- conf.d/halostatue_fish_chezmoi.fish
+++ conf.d/halostatue_fish_chezmoi.fish
@@ -1,12 +1,16 @@
-# @halostatue/fish-chezmoi/conf.d/halostatue_fish_chezmoi.fish:v1.0.0
+# @halostatue/fish-chezmoi/conf.d/halostatue_fish_chezmoi.fish:v1.1.0
 
 function _halostatue_fish_chezmoi_setup
     status --is-interactive
     or return
 
+    switch $halostatue_fish_chezmoi_completion_mode
+        case never
+            return
+    end
+
     set chezmoi (command --search chezmoi)
     or return
-
 
     if set --query XDG_DATA_HOME
         set --function local_completions $XDG_DATA_HOME
@@ -16,28 +20,31 @@ function _halostatue_fish_chezmoi_setup
 
     set local_completions $local_completions/fish/vendor_completions.d
 
+    if test $chezmoi -nt $local_completions/chezmoi.fish
+        rm -f $local_completions/chezmoi.fish
+    end
+
+    set --function completion (
+      path filter --type file $fish_complete_path/chezmoi.fish
+    )[1]
+
+    if not set --query completion[1] || test $chezmoi -nt $completion
+        set --function refresh 1
+    end
+
     switch $halostatue_fish_chezmoi_completion_mode
-        case save
-        case '*'
+        case source
             $chezmoi completion fish | source
-            return
-    end
+        case save
+            if set --query refresh
+                mkdir -p $local_completions
+                and $chezmoi completion fish >$local_completions/chezmoi.fish
+            end
 
-    set --function completion (path filter --type f $fish_complete_path/chezmoi.fish)
-
-    if set --query completion[1]
-        if test $chezmoi -nt $completion[1]
-            set --function save 1
-        end
-    else
-        set --function save 1
-    end
-
-    if set --query save
-        mkdir -p $local_completions
-        $chezmoi completion fish >$local_completions/chezmoi.fish
-    else
-        $chezmoi completion fish | source
+        case default '' '*'
+            if set --query refresh
+                $chezmoi completion fish | source
+            end
     end
 end
 
